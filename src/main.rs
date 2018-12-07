529dbfe74cd43a7ecd4a4973dde7a1c2529da1b0
ehcayen <yanyi.x.chen@ericsson.com>
2018-12-07 17:46:19 +0800

    fixed error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![feature(core_intrinsics)]
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", unsafe { std::intrinsics::type_name::<T>() });
+}
+
 fn main() {
-    let _: bool = 4u32 / 3u32;
+    let _array1 = ['x', 'y', 'z'];
+    let _array2 = ['x', 'y'];
+
+    let _vector1: Vec<char> = vec!['x', 'y', 'z'];
+    let _vector2: Vec<char> = vec!['x', 'y'];
+
+    print_type_of(&_array1);
+    print_type_of(&_array2);
+    print_type_of(&_vector1);
+    print_type_of(&_vector2);
 }
