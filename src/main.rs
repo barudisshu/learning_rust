4c6f05befb9ba2a166c0462d56cebc1cf8500539
ehcayen <yanyi.x.chen@ericsson.com>
2018-12-07 17:18:35 +0800

    void type & constants variable

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-
-
 fn main() {
-    let _a: i16 = -150;
-    let _b = -150 as i16;
-    let _c = -150 + _b - _b;
-    let _d = -150i16;
-    let _e = -150_i16;
-    let _f = 5__u32;
+    let n = 20;
+    let n = 6;;
+    println!("{}", n);
 }
