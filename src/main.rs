1276ceb75b7065f3c050da8b828b3408d21ed3bd
ehcayen <yanyi.x.chen@ericsson.com>
2018-12-29 15:22:26 +0800

    chapter 10 starting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 fn main() {
     let mut arr = [5, -4, 9, 0, -7, -1, 3, 5, 3, 1];
-    double_negatives(arr);
-    print!("{:?}", arr);
-}
+    double_negatives(&mut arr);
+    println!("{:?}", arr);
 
-fn double_negatives(mut a: [i32; 10]) {
+    let mut a = 3;
+    plus(&mut a);
+    println!("{:?}", a);
+}
+fn double_negatives(a: &mut [i32; 10]) {
     for i in 0..10 {
         if a[i] < 0 { a[i] *= 2; }
     }
+}
+fn plus(a: &mut i32) {
+    let factor:&i32 = &2;
+    *a += factor
 }
