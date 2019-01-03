63cc5bf37be36766812fd309e54e0c2e2338ae76
ehcayen <yanyi.x.chen@ericsson.com>
2019-01-03 14:03:23 +0800

    feat(chap11#3): Stack Allocation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::panic;
 
 fn main() {
-    static _A: u32 = 3;
-    static _B: i32 = -1_000_000;
-    static _C: f64 = 5.7e10;
-    static _D: u8  = 200;
+    let _a: u32 = 3;
+    let _b: i32 = -1_000_000;
+    let _c: f64 = 5.7e10;
+    let _d: u8  = 200;
 }
 
