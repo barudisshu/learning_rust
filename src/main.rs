9c08430092ddcb41ae49ae813f682a4a4c77fe5c
Galudisu <galudisu@gmail.com>
2019-01-09 23:31:45 +0800

    feat(chap12#6): Sizes of Composite Data Types

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use std::mem::*;
+
+///
+/// 部分组合类型的情况会出现结构填充(padding)，造成内存浪费。
 fn main() {
-    let a = 7;
-    let mut a_box: Box<i32>;
-    let a_ref: &i32 = &a;
-    print!("{} {};", a, a_ref);
-    a_box = Box::new(a + 2);
-    print!(" {} {} {};", a, a_ref, a_box);
-    a_box = Box::new(*a_ref);
-    print!(" {} {} {}", a, a_ref, a_box);
+    print!("{} {} {} {} {} {}",
+           size_of_val(&[0i16; 80]),
+           size_of_val(&(0i16, 0i64)),
+           size_of_val(&[(0i16, 0i64); 100]),
+           size_of_val(&E1::_E1a),
+           size_of_val(&E2::_E2a),
+           size_of_val(&vec![(0i16, 0i64); 100]));
 }
+
+enum E1 {
+    _E1a,
+    _E1b,
+}
+
+enum E2 {
+    _E2a,
+    _E2b(f64),
+}
+
