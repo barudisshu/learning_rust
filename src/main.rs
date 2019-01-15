b52b8aabe509ca749f4517a28b52f084579bfb8b
Galudisu <galudisu@gmail.com>
2019-01-15 23:47:16 +0800

    feat(chap13#7): Vector Allocation

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,14 @@ use std::mem::*;
 ///
 /// 部分组合类型的情况会出现结构填充(padding)，造成内存浪费。
 fn main() {
-    print!("{} {} {} {} {} {}",
-           size_of_val(&[0i16; 80]),
-           size_of_val(&(0i16, 0i64)),
-           size_of_val(&[(0i16, 0i64); 100]),
-           size_of_val(&E1::_E1a),
-           size_of_val(&E2::_E2a),
-           size_of_val(&vec![(0i16, 0i64); 100]));
+    let mut v = vec![0; 0];
+    let mut prev_capacity = std::usize::MAX;
+    for i in 0..1_000 {
+        let cap = v.capacity();
+        if cap != prev_capacity {
+            println!("{} {} {}", i, v.len(), cap);
+            prev_capacity = cap;
+        }
+        v.push(1);
+    }
 }
-
-enum E1 {
-    _E1a,
-    _E1b,
-}
-
-enum E2 {
-    _E2a,
-    _E2b(f64),
-}
-
