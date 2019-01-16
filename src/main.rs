d3bfbc06c067f2983e48c2cc387bfe6ae7a42a4b
ehcayen <yanyi.x.chen@ericsson.com>
2019-01-16 18:10:12 +0800

    feat(chap13#2): Closures

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-use std::mem::*;
+use std::cmp::Ordering;
 
 ///
-/// 部分组合类型的情况会出现结构填充(padding)，造成内存浪费。
 fn main() {
-    let mut v = vec![0; 0];
-    let mut prev_capacity = std::usize::MAX;
-    for i in 0..1_000 {
-        let cap = v.capacity();
-        if cap != prev_capacity {
-            println!("{} {} {}", i, v.len(), cap);
-            prev_capacity = cap;
-        }
-        v.push(1);
-    }
+    let mut arr = [4, 8, 1, 10, 0, 45, 12, 7];
+    arr.sort_by(|a, b|
+        if a < b { Ordering::Greater }
+        else if a > b { Ordering::Less }
+        else { Ordering::Equal }
+    );
+    print!("{:?}", arr);
 }
