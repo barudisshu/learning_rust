4b80ce0fa18650954a53d5c4275fa1fd67f81e77
Galudisu <galudisu@gmail.com>
2019-01-25 00:08:23 +0800

    feat(chap15#5): Out-of-Range Slicing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
-use std::mem::*;
-
 ///
 fn main() {
-    print!("{} ", min(&[23, 17]));
-    print!("{} ", min(&vec![55, 22, 33, 44]));
-}
-
-fn min(arr: &[i32]) -> i32 {
-    // Let's assume 'arr' is not empty.
-    let mut minimum = arr[0];
-    for i in 1..arr.len() {
-        if arr[i] < minimum { minimum = arr[i]; }
-    }
-    minimum
+    let arr = [55, 22, 33, 44, 66, 7, 8];
+    let v = vec![55, 22, 33, 44, 66, 7, 8];
+    let sr1 = &arr[2..5];
+    let sr2 = &v[2..5];
+    print!("{:?} {:?} {:?} {:?}", sr1, sr2, &sr1[1..2], &sr1[1]);
 }
