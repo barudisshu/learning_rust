dc8dead5b9637af4b81e9c59c04b9d2424470def
ehcayen <yanyi.x.chen@ericsson.com>
2019-02-13 14:10:37 +0800

    If there is no data sink, there is no data access.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,11 @@ extern crate t_bang;
 use t_bang::*;
 
 fn main() {
-    let arr = [66, -8, 43, 19, 0, -32];
-    let v = arr
-    .iter()
-    .filter(|x| **x > 0)
-    .map(|x| *x * 2)
-    .collect::<Vec<_>>();
-    print!("{:?}", v);    
-    print!("{:?}", t!(v));
+    let v = [66, -8, 43, 19, 0, -31]
+        .iter()
+        .filter(|x| { print!("F{} ", x); **x > 0 })
+        .map(|x| { print!("M{} ", x); *x * 2 })
+        .collect::<Vec<_>>();
+    print!("{:?}", v);
 }
 
