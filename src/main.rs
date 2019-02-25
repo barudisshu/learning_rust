464f86bf7cf66a841576fa56a3d830064b29acec
ehcayen <yanyi.x.chen@ericsson.com>
2019-02-25 13:56:57 +0800

    feat(chap19#3): different crate cant be both outside  the crate

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,15 @@ use std::io::Write;
 
 use t_bang::*;
 
-struct Stru {
-    x: u16,
-    y: u16,
-}
-impl Stru {
-    fn f1(a: u32) -> bool {
-        a == 0
-    }
-    fn f2(&self, b: u16) -> Self {
-        if b == self.x || b == self.y {
-            Stru {
-                x: self.x + 1,
-                y: self.y + 1,
-            }
-        } else {
-            Stru {
-                x: self.x - 1,
-                y: self.y - 1,
-            }
-        }
+struct Pair(u32, u32);
+impl std::iter::Iterator for Pair {
+    type Item = u32;
+    fn next(&mut self) -> Option<Self::Item> {
+        None
     }
 }
 
 fn main() {
-    let s = Stru { x: 23, y: 456 };
-    print!("{} {}", Stru::f1(500_000), s.f2(456).x);
+    let mut a = Pair(23u32, 34u32);
+    print!("{:?}", a.next());
 }
